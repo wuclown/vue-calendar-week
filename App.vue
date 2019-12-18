fc8d1adf1ff6a17e56820db604ef3f109476e424
wyj <1500421902@qq.com>
2019-12-18 11:18:16 +0800

    vue 滑动周日历组件

---
+++ App.vue
@@ -0,0 +1,23 @@
+<!--
+ * @Author: wyj
+ * @Date: 2019-11-12 22:12:13
+ * @LastEditors  : wyj
+ * @LastEditTime : 2019-12-18 10:27:24
+ * @Description: 
+ -->
+<template>
+    <VueCalendarWeek />
+</template>
+<script>
+import VueCalendarWeek from "./app/index.js"
+export default {
+    components: {
+        VueCalendarWeek
+    },
+    data() {
+        return {
+            value: 0
+        }
+    }
+}
+</script>
